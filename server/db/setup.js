aae38b1096d5f94b89768ae51237215201d7dc80
Comfort AJala <comfort.ajala@ajalacomfort.com>
2017-08-12 12:21:55 +0200

    removed db connection

--- server/db/setup.js
+++ server/db/setup.js
@@ -2,7 +2,10 @@ var path = require("path"),
 mongoose = require("mongoose"),
 config = require(path.resolve(__dirname, "..", "config", "config"))
 
-module.exports =  ()=>{
+/*module.exports =  ()=>{
           var dbconnection= mongoose.connect(config.DB_NAME, ()=>console.log("Database Connected at ", config.DB_NAME))
           return dbconnection
 }
+*/
+
+console.log("Stoppped mongo connection")
