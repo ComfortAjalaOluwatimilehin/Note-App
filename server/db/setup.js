f9758f93be8949113408500743388ed3200393f7
Comfort AJala <comfort.ajala@ajalacomfort.com>
2017-08-14 01:28:23 +0200

    console for DB

--- server/db/setup.js
+++ server/db/setup.js
@@ -2,7 +2,7 @@ var path = require("path"),
 mongoose = require("mongoose"),
 config = require(path.resolve(__dirname, "..", "config", "config"))
 
-module.exports =  ()=>{
+module.exports =function(){
           var dbconnection= mongoose.connect(config.DB_NAME, ()=>console.log("Database Connected at ", config.DB_NAME))
           return dbconnection
 
